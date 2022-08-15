003cdadd9a4021a2ec6d8fd9c31527f9780ea440
Aslopow <111162413+Aslopow@users.noreply.github.com>
2022-08-15 10:05:09 +0800

    Create Picture Download.js

---
+++ Android Js/Picture Download.js
@@ -0,0 +1,8 @@
+var url = "https://avatars.githubusercontent.com/u/111162413?v=4";
+var res = http.get(url);
+if (res.statusCode != 200) {
+    toast("请求失败 Error-0");
+}
+files.writeBytes("/sdcard/1.png", res.body.bytes());
+toast("下载成功 Finish");
+app.viewFile("/sdcard/Main.png");
